11888ecc7bf499d63dea43c73f19b3bd17f2d2d0
Lucas Vinicius <lucasvinipessoal@gmail.com>
2023-12-17 17:09:59 -0300

    start

---
+++ my-portifolio/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import styles from './page.module.scss'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <nav className={styles.navbar}>
+        
+      </nav>
+    </main>
+  )
+}
